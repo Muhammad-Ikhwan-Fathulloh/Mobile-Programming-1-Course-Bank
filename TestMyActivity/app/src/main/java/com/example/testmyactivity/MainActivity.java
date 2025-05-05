0e63698b1d3a942830f76133f2bb2fd8362f84b6
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2025-05-05 11:04:28 +0700

    Update activiy

--- TestMyActivity/app/src/main/java/com/example/testmyactivity/MainActivity.java
+++ TestMyActivity/app/src/main/java/com/example/testmyactivity/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testmyactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonHome = findViewById(R.id.buttonHomeActivity);
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
