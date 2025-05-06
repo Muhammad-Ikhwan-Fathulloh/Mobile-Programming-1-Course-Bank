57d66891f690ded5e7fa075548408e7fffbf67b7
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2025-05-06 16:28:16 +0700

    Update Activity

---
+++ TestActivity/app/src/main/java/com/example/testactivity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.testactivity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button buttonActivity = (Button) findViewById(R.id.button_activity);
+        Log.d("Test", "Hello");
+        buttonActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("BUTTONS", "Button Click");
+            }
+        });
+    }
+}
